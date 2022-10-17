0049e797eaaef76c936e9a670dd239d3765bcfbd
Stnslvvlbv <stnslvvlbv@gmail.com>
2022-10-18 00:57:39 +0700

    daily sort

--- react-app/src/components/block/DPrice.jsx
+++ react-app/src/components/block/DPrice.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import {DPrice__long} from './DPrice__long'
 
 const DPrice = (props) => {
-  const style = ["dprice"]
-  console.log(props.dailyFutures);
 
-
-  // let ArayResponse = new Array();
-
-  // console.log(response);
-  // alert(response);
+  const amountCoins = 10;
+  const style = ["dprice"];
+  const Sort = "priceChangePercent";
+  let dailyLong = [];
+  let dailyShort = [];
+  const sortCoin = (Sort) => {
+    const DailyActive = JSON.parse(JSON.stringify(props.dailyFutures))
+    DailyActive.forEach(item => {
+      item[Sort] = Number(item[Sort]);
+    })
+    DailyActive.sort((a, b) => a[Sort] > b[Sort] ? 1 : -1);
+    console.log('sorted');
+    dailyShort = DailyActive.slice(0, amountCoins);
+    dailyLong = DailyActive.slice(DailyActive.length - amountCoins, DailyActive.length)
+    dailyLong.sort((a, b) => a[Sort] < b[Sort] ? 1 : -1);
+  };
+  sortCoin(Sort);
+  console.log(dailyLong);
+  console.log(dailyShort);
 
   return(
     <div className="dprice">
@@ -19,7 +31,7 @@ const DPrice = (props) => {
       </div>
       <div className="dprice__coins">
         <div className="dprice__long">
-        {props.dailyFutures.map(coin =>
+        {dailyLong.map(coin =>
           <DPrice__long key = {coin.symbol}
             symbol = {coin.symbol}
             lastPrice = {coin.lastPrice}
--- react-app/src/components/block/DPrice__long.jsx
+++ react-app/src/components/block/DPrice__long.jsx
@@ -5,7 +5,7 @@ const DPrice__long = (props) => {
   return (
     <div className="dprice__long-element">
       <h1>{props.symbol}</h1>
-      <p>{props.lastPrice} {props.priceChangePercent}%</p>
+      <h1>{props.lastPrice} {props.priceChangePercent}%</h1>
     </div>
   )
 }
