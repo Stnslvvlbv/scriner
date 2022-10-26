26f76ce1a116b40e61f81cf80ca4e7667840e039
Stnslvvlbv <stnslvvlbv@gmail.com>
2022-10-26 23:04:38 +0700

    s

--- react-app/src/components/Canvas/TraideSessionsCanvas.jsx
+++ react-app/src/components/Canvas/TraideSessionsCanvas.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRef, useEffect, } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 
 const TraideSessionsCanvas = () => {
@@ -7,36 +7,49 @@ const TraideSessionsCanvas = () => {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
 
+  const [isDrawing, setIsDrawing] = useState();
+
   useEffect(() =>{
     const canvas =canvasRef.current;
     canvas.width = 500;
     canvas.height = 500;
 
     const context = canvas.getcontext("2d");
-    content.lineCap = "round";
+    context.lineCap = "round";
     context.strokeStile = "black";
     context.lineWidth = 5;
-    contextRef.current = contect;
+    contextRef.current = context;
   }, []);
 
   const startDrawing = (nativeEvent) => {
     const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+    setIsDrawing(true);
+    nativeEvent.preventDefault();
   }
 
-  const draw = () => {
-
+  const draw = (nativeEvent) => {
+    if (!isDrawing) {
+      return;
+    }
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+    nativeEvent.preventDefault();
   }
 
-  const stopDrawing = () => {
+  const stopDrawing = (nativeEvent) => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
 
   }
 
   return (
     <canvas className='TraideSessionsCanvas'
       ref={canvasRef}
-      onMouseDown={startDrawing}
-      onMouseMove={draw}
-      onMouseUp={stopDrawing}
     >
       dfthjhlk
     </canvas>
