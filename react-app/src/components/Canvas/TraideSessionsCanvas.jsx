b4c70084217fe166cab0e0ab80c26960d51479ba
Stnslvvlbv <stnslvvlbv@gmail.com>
2022-10-27 08:51:33 +0700

    mouse dowm & up

--- react-app/src/components/Canvas/TraideSessionsCanvas.jsx
+++ react-app/src/components/Canvas/TraideSessionsCanvas.jsx
@@ -1,49 +1,58 @@
 import React from 'react';
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState, useCallback } from 'react';
 
 
 const TraideSessionsCanvas = () => {
 
-  const canvasRef = useRef();
-  const ctx = useRef();
+  const canvasRef = useRef(null);
+  const ctx = useRef(null);
 
-  const [isDrawing, setIsDrawing] = useState();
-
-
-  const draw = (x, y) => {
-    ctx.beginPath();
-    ctx.current.strokStyle = 'black';
-    ctx.current.lineWidth = 10;
-    ctx.current.lineJoin = 'round';
-    ctx.current.moveTo(lastPosition.x, lastPosition.y)
-    ctx.current.lineTo(x, y);
-    ctx.current.closePath();
-    ctx.current.stroke();
-  }
-
+  const [mouseDown, setMouseDown] = useState(false);
+  const [lastPosition, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
 
   useEffect(() =>{
     console.log(canvasRef);
     if (canvasRef.current) {
       ctx.current = canvasRef.current.getContext('2d');
-
     }
   }, []);
 
-  const startDrawing = (nativeEvent) => {
+  const draw = useCallback((x, y) => {
+    if (mouseDown) {
+        ctx.current.beginPath();
+        ctx.current.strokStyle = 'black';
+        ctx.current.lineWidth = 10;
+        ctx.current.lineJoin = 'round';
+        ctx.current.moveTo(lastPosition.x, lastPosition.y)
+        ctx.current.lineTo(x, y);
+        ctx.current.closePath();
+        ctx.current.stroke();
+        setPosition({x, y});
+    }
+  }, [lastPosition, mouseDown, setPosition])
 
+  const onMouseDown = (e) => {
+    setPosition({
+      x: e.pageX,
+      y: e.pageY
+    });
+    setMouseDown(true);
   }
 
-  const stopDrawing = (nativeEvent) => {
-
+  const onMouseUp = (e) => {
+    setMouseDown(false);
   }
-
+  console.log(mouseDown, lastPosition)
   return (
     <canvas className='TraideSessionsCanvas'
       ref={canvasRef}
-      onMouseDown={startDrawing}
+      onMouseDown={onMouseDown}
       onMouseMove={draw}
-      onMouseUp={stopDrawing}
+      onMouseUp={onMouseUp}
+      onMouseLeave={onMouseUp}
     >
       dfthjhlk
     </canvas>
